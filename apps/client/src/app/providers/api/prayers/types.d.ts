8673791e9114b3725952664026682d1c42d80d64
KayanHendges <kayanbhendges@gmail.com>
2023-08-15 16:10:57 -0300

    fix: create prayer types

--- apps/client/src/app/providers/api/prayers/types.d.ts
+++ apps/client/src/app/providers/api/prayers/types.d.ts
@@ -1,6 +1,6 @@
 import { Prayer } from "project-types";
 
 interface CreatePrayerPayload
-  extends Omit<Prayer, "id" | "cleanBody" | "updatedAt" | "createdAt"> {}
+  extends Omit<Prayer, "id" | "cleanBody" | 'cleanDescription' | "updatedAt" | "createdAt"> {}
 
 interface UpdatePrayerPayload extends Partial<CreatePrayerPayload> {}

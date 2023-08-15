5149527618d0ada4535b71db8fa6f9c1d71c6ac1
KayanHendges <kayanbhendges@gmail.com>
2023-08-15 09:38:37 -0300

    update: favicon

--- apps/client/src/app/(site)/layout.tsx
+++ apps/client/src/app/(site)/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt_BR" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body
         className={twMerge(
           inter.className,

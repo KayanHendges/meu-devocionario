68adbd3ef583afe3e6ab97dc730b69258fab6501
KayanHendges <kayanbhendges@gmail.com>
2023-08-17 08:18:41 -0300

    chore: description container

--- apps/client/src/app/components/Html/HtmlDisplay/index.tsx
+++ apps/client/src/app/components/Html/HtmlDisplay/index.tsx
@@ -1,19 +1,23 @@
-import { ComponentProps } from "react";
+import { ComponentProps, forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface Props extends ComponentProps<"div"> {
   children: string;
 }
 
-export default function HtmlDisplay({ children, className, ...props }: Props) {
-  return (
-    <div
-      className={twMerge(
-        "prose-zinc dark:prose-invert",
-        className
-      )}
-      dangerouslySetInnerHTML={{ __html: children }}
-      {...props}
-    />
-  );
-}
+const HtmlDisplay = forwardRef<HTMLDivElement, Props>(
+  ({ children, className, ...props }, ref) => {
+    return (
+      <div
+        className={twMerge("prose-zinc dark:prose-invert", className)}
+        dangerouslySetInnerHTML={{ __html: children }}
+        {...props}
+        ref={ref}
+      />
+    );
+  }
+);
+
+HtmlDisplay.displayName = "HtmlDisplay";
+
+export default HtmlDisplay;
--- apps/client/src/app/components/Texts/Text.tsx
+++ apps/client/src/app/components/Texts/Text.tsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import { Slot } from "@radix-ui/react-slot";
-import { ComponentProps } from "react";
+import { ComponentProps, forwardRef } from "react";
 
 export interface TextProps extends ComponentProps<"span"> {
   size?: "sm" | "md" | "lg" | "xl";
@@ -8,35 +8,44 @@ export interface TextProps extends ComponentProps<"span"> {
   truncate?: boolean;
 }
 
-export function Text({
-  size = "md",
-  children,
-  asChild,
-  className,
-  truncate = true,
-  title,
-  ...props
-}: TextProps) {
-  const Component = asChild ? Slot : "span";
+const Text = forwardRef<HTMLSpanElement, TextProps>(
+  (
+    {
+      size = "md",
+      children,
+      asChild,
+      className,
+      truncate = true,
+      title,
+      ...props
+    },
+    ref
+  ) => {
+    const Component = asChild ? Slot : "span";
 
-  return (
-    <Component
-      className={clsx(
-        "font-sans text-zinc-900 dark:text-zinc-200",
-        {
-          truncate,
-          "text-xs": size === "sm",
-          "text-sm": size === "md",
-          "text-md": size === "lg",
-          "text-lg": size === "xl",
-        },
-        className
-      )}
-      title={title}
-      {...props}
-      ref={undefined}
-    >
-      {children}
-    </Component>
-  );
-}
+    return (
+      <Component
+        className={clsx(
+          "font-sans text-zinc-900 dark:text-zinc-200",
+          {
+            truncate,
+            "text-xs": size === "sm",
+            "text-sm": size === "md",
+            "text-md": size === "lg",
+            "text-lg": size === "xl",
+          },
+          className
+        )}
+        title={title}
+        {...props}
+        ref={ref}
+      >
+        {children}
+      </Component>
+    );
+  }
+);
+
+Text.displayName = "Text";
+
+export { Text };
